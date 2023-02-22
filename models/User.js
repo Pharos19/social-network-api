cf0c4148aa878ae98a552afa9e06f2d350ebe512
Pharos19 <Pharos199@gmail.com>
2023-02-22 14:17:54 -0700

    updated user.js

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model, Types } = require('mongoose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                'Enter a valid email address',
+            ],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
+const User = model('User', userSchema)
+
+module.exports = User
